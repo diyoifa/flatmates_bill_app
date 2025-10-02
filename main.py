1795ec23571281ff747c393498ba033d220a2ad9
diyoifa <dani12ca12@gmail.com>
2025-10-02 16:30:50 -0500

    Feat Open pdf file
    once you run the program the pdf would be open

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import webbrowser
+import os
 from fpdf import FPDF
 
 
@@ -24,7 +26,7 @@ class Flatmate:
     def pays(self, bill, flatmate):
         total_days = self.days_in_house + flatmate.days_in_house
         weight = self.days_in_house/total_days
-        return  weight * bill.amount
+        return  round(weight * bill.amount,2)
 
 class PdfReport:
     """
@@ -39,25 +41,30 @@ class PdfReport:
         pdf = FPDF(orientation='P', unit='pt', format='A4')
         pdf.add_page()
 
-        #Insert title
-        pdf.set_font('Arial', 'B', 22)
-        pdf.cell(0, 80, txt="Flatmates Bill", border=1, align="C", ln=1)
+        #Add icon image
+        pdf.image("house.png", w=30, h=30)
 
-        #Insert label and value
-        pdf.cell(80, 40, txt="period", border=1)
-        pdf.cell(150, 40, txt=bill.period, border=1, ln=1)
+        #Insert title
+        pdf.set_font(family='Arial', style='B', size=22)
+        pdf.cell(0, 80, txt="Flatmates Bill", align="C", ln=1)
+
+        #Insert Period label and value
+        pdf.set_font(family='Arial', style='B', size=14,)
+        pdf.cell(80, 40, txt="Period: ")
+        pdf.cell(150, 40, txt=bill.period, ln=1)
 
         #Insert name and due amount of the first flatmate
-        pdf.cell(80, 40, txt=flatmate1.name, border=1)
-        pdf.cell(80, 40, txt=str(flatmate1.pays(bill, flatmate2)), border=1)
+        pdf.set_font(family='Arial', size=14)
+        pdf.cell(80, 40, txt=flatmate1.name)
+        pdf.cell(80, 40, txt=str(flatmate1.pays(bill, flatmate2)), ln=1)
 
         #Insert name and due amount of the second flatmate
-        pdf.cell(80, 40, txt=flatmate2.name, border=1)
-        pdf.cell(80, 40, txt=str(flatmate2.pays(bill, flatmate1)), border=1)
+        pdf.cell(80, 40, txt=flatmate2.name)
+        pdf.cell(80, 40, txt=str(flatmate2.pays(bill, flatmate1)),)
 
         #Save pdf
         pdf.output(self.filename)
-
+        webbrowser.open('file:///'+os.path.realpath(self.filename))
 
 bill = Bill(amount=10000, period="March 2025")
 john = Flatmate(name="John", days_in_house=12)
